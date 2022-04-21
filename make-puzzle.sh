47fa4fbc9b19834db5150f547705ede119a7a9a8
Joel Johnson <joel@j2i.net>
2022-04-21 15:32:38 -0400

    added metadata to output

--- make-puzzle.sh
+++ make-puzzle.sh
@@ -1,4 +1,6 @@
 
+count() { echo $#; }
+
 INPUT_FILE=$1
 OUTPUT_FILE=$2
 
@@ -14,15 +16,30 @@ pushd working
 
 pwd;
 find . -maxdepth 1 -type f  -print0 | while IFS= read -r -d $'\0' file; do
-    ../clean-puzzle/clean-puzzle "$file" >> "clean/$file.clean"  
+    ../clean-puzzle/clean-puzzle "$file" >> "clean/$file.clean"
 done
 
 cd clean
+index=0
+
+declare -a fileList
+
 find . -maxdepth 1 -type f  -print0 | while IFS= read -r -d $'\0' file; do
-    genxword "$file" --output $OUTPUT_FILE s --auto
+	PUZZLE_NAME="$OUTPUT_FILE.$index"
+	echo "Index $index"
+	fileList[$index]="$PUZZLE_NAME"
+	echo "File: ${fileList[$index]}"
+	genxword "$file" --output $PUZZLE_NAME s --auto
+	index=$((index+1))
 done
 
+echo"x:$x";
 
+echo "making json"
+echo "{'baseName': '$OUTPUT_FILE'}" > description.json
+echo "]}"
+
+echo  ${fileList[*]}
 
 popd
 
